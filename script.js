c10e8afb958c5e186021d2cbc4dc21ee035a701e
dimasyoga42 <dimasyoga9090@gmail.com>
2024-05-28 02:28:04 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+new TypeIt("#title", {
+  strings: "Qur'an Ku",
+  speed: 85,
+  loop: true,
+  waitUntilVisible: true,
+}).go();
+
+function updateClock() {
+   const now = new Date();
+   const hours = now.getHours().toString().padStart(2, '0');
+   const minutes = now.getMinutes().toString().padStart(2, '0');
+   const seconds = now.getSeconds().toString().padStart(2, '0');
+
+   const timeString = `${hours}:${minutes}:${seconds}`;
+
+   document.getElementById('clock').textContent = timeString;
+ }
+
+ setInterval(updateClock, 1000);
+ updateClock();  // Initialize clock immediately
---
+++ src/pages/ayat.js
@@ -0,0 +1,54 @@
+async function createButtons() {
+  const buttonContainer = document.querySelector('.button-container');
+  try {
+    const response = await fetch('https://quran-api.santrikoding.com/api/surah');
+    const data = await response.json();
+    console.log(data);
+
+    let rowDiv = document.createElement('div');
+    rowDiv.classList.add('row');
+
+    data.forEach((surah, index) => {
+      const colDiv = document.createElement('div');
+      colDiv.classList.add('col-sm-6', 'mb-3', 'mb-sm-0');
+
+      colDiv.innerHTML = `
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">${surah.nama_latin}</h5>
+                        <p class="card-text">${surah.nama_latin} yang berarti ${surah.arti} surah ini di turunkan di ${surah.tempat_turun} dan memiliki jumlah ayat ${surah.jumlah_ayat}</p>
+                        <a href="#" class="btn" data-number="${surah.nomor}">Read</a>
+                    </div>
+                </div>
+            `;
+
+      rowDiv.appendChild(colDiv);
+
+      // Add a new row after every two columns
+      if ((index + 1) % 2 === 0) {
+        buttonContainer.appendChild(rowDiv);
+        rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+      }
+    });
+
+    // Append the last row if it has elements
+    if (rowDiv.hasChildNodes()) {
+      buttonContainer.appendChild(rowDiv);
+    }
+
+    // Add event listeners to the buttons
+    document.querySelectorAll('.btn').forEach(button => {
+      button.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent default anchor behavior
+        const surahNumber = button.getAttribute('data-number');
+        window.location.href = `surah.html?number=${surahNumber}`;
+      });
+    });
+
+  } catch (error) {
+    console.error('Error fetching surah data:', error);
+  }
+}
+
+createButtons();
